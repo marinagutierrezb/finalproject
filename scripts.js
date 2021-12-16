d4a8b7f95be654ee231e2cd0b14ed420d1a2b40e
marina <marinagutierrez1999@gmail.com>
2021-12-16 05:02:30 +0000

    Upload of game files

--- scripts.js
+++ scripts.js
@@ -1,143 +1,63 @@
-const statusDiv = document.querySelector('.status');
-const resetDiv = document.querySelector('.reset');
-const cellDivs = document.querySelectorAll('.cell');
+const cards = document.querySelectorAll('.memory-card');
 
-// constants //
-const xSymbol = 'X';
-const oSymbol = 'O';
-// constants //
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
-let gameIsLive = true;
-let xIsNext = true;
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    
+    
+   this.classList.add('flip');
+    
+    if (!hasFlippedCard) {
+        //first time the card was clicked
+        hasFlippedCard = true;
+        firstCard = this;
+        
+        return;
+    } 
+    
+        //second click check
+        secondCard = this;
 
-
-// functions //
-const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
-
-const handleWin = (letter) => {
-  gameIsLive = false;
-  if (letter === 'x') {
-    statusDiv.innerHTML = `${letterToSymbol(letter)} IS THE WINNER!`;
-  } else {
-    statusDiv.innerHTML = `<span>${letterToSymbol(letter)} IS THE WINNER!</span>`;
-  }
-};
-// function //
-
-// status key //
-const checkGameStatus = () => {
-  const topLeft = cellDivs[0].classList[1];
-  const topMiddle = cellDivs[1].classList[1];
-  const topRight = cellDivs[2].classList[1];
-  const middleLeft = cellDivs[3].classList[1];
-  const middleMiddle = cellDivs[4].classList[1];
-  const middleRight = cellDivs[5].classList[1];
-  const bottomLeft = cellDivs[6].classList[1];
-  const bottomMiddle = cellDivs[7].classList[1];
-  const bottomRight = cellDivs[8].classList[1];
-	// status key //
-	
-
-  // win status //
-  if (topLeft && topLeft === topMiddle && topLeft === topRight) {
-    handleWin(topLeft);
-    cellDivs[0].classList.add('won');
-    cellDivs[1].classList.add('won');
-    cellDivs[2].classList.add('won');
-		
-  } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
-    handleWin(topLeft);
-    cellDivs[0].classList.add('won');
-    cellDivs[3].classList.add('won');
-    cellDivs[6].classList.add('won');  
-		
-	} else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
-    handleWin(topLeft);
-    cellDivs[0].classList.add('won');
-    cellDivs[4].classList.add('won');
-    cellDivs[8].classList.add('won');
-		
-	} else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
-    handleWin(topRight);
-    cellDivs[2].classList.add('won');
-    cellDivs[4].classList.add('won');
-    cellDivs[6].classList.add('won');
-		
-	} else if (topRight && topRight === middleRight && topRight === bottomRight) {
-    handleWin(topRight);
-    cellDivs[2].classList.add('won');
-    cellDivs[5].classList.add('won');
-    cellDivs[8].classList.add('won');
-		
-	} else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
-    handleWin(middleLeft);
-    cellDivs[3].classList.add('won');
-    cellDivs[4].classList.add('won');
-    cellDivs[5].classList.add('won');
-		
-  } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
-    handleWin(topMiddle);
-    cellDivs[1].classList.add('won');
-    cellDivs[4].classList.add('won');
-    cellDivs[7].classList.add('won');
-		
-		} else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
-    handleWin(bottomLeft);
-    cellDivs[6].classList.add('won');
-    cellDivs[7].classList.add('won');
-    cellDivs[8].classList.add('won');	
-
-		
-
-		
-  } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
-    gameIsLive = false;
-    statusDiv.innerHTML = 'TIED GAME!';
-  } else {
-    xIsNext = !xIsNext;
-    if (xIsNext) {
-      statusDiv.innerHTML = `${xSymbol} 's Turn`;
-    } else {
-      statusDiv.innerHTML = `<span>${oSymbol} 's Turn</span>`;
-    }
-  }
-};
-// win status //
-
-// options clickers //
-const handleReset = () => {
-  xIsNext = true;
-  statusDiv.innerHTML = `${xSymbol} 's Turn`;
-  for (const cellDiv of cellDivs) {
-    cellDiv.classList.remove('x');
-    cellDiv.classList.remove('o');
-    cellDiv.classList.remove('won');
-  }
-  gameIsLive = true;
-};
-
-const handleCellClick = (e) => {
-  const classList = e.target.classList;
-
-  if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {
-    return;
-  }
-
-  if (xIsNext) {
-    classList.add('x');
-    checkGameStatus();
-  } else {
-    classList.add('o');
-    checkGameStatus();
-  }
-};
-
-
-resetDiv.addEventListener('click', handleReset);
-
-for (const cellDiv of cellDivs) {
-  cellDiv.addEventListener('click', handleCellClick)
+        checkForMatch();
+    
 }
-// option clicker //
+    
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
 
-// adapted from [https://www.youtube.com/watch?v=Rzhcb4M9-0Q] //
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+        
+    resetBoard();
+    }, 1200);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 16);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
